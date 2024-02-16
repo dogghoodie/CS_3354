8f615aa0ef50406e65962fa68cf5923b878d8205
dogghoodie <dogghoodie@gmail.com>
2024-02-16 05:04:38 -0600

    Comments

--- 1/src/main/java/InputHandler.java
+++ 1/src/main/java/InputHandler.java
@@ -3,12 +3,15 @@ import java.util.NoSuchElementException;
 
 public class InputHandler {
 	public String scan() {
+		// Scans user input 
 		try {
 			Scanner scanner = new Scanner(System.in);
 			return scanner.nextLine();
+		// Checks for exception
 		} catch (NoSuchElementException e) {
 			System.err.println("Error: Scanner exhausted unexpectedly.");
 		}
+	// Default return case
 	return "";
 	}
 }
