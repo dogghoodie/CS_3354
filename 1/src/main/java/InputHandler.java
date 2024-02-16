ddcec76c712605fdc0ee3e4d7b48ccf95457ee8f
dogghoodie <dogghoodie@gmail.com>
2024-02-16 01:58:07 -0600

    ?

---
+++ 1/src/main/java/InputHandler.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class InputHandler {
+	public String scan() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextLine();
+
+	}
+}
--- 1/src/main/java/JavaJSON.java
+++ 1/src/main/java/JavaJSON.java
@@ -4,10 +4,12 @@ import java.io.FileNotFoundException;
 import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONPaser;
+import org.json.simple.parser.JSONParser;
 
 class JavaJSON {
 	public static void main(String[] args) {
-		
+		InputHandler input = new InputHandler();
+		String data = input.scan();
+		System.out.println(data);
 	}
 }
