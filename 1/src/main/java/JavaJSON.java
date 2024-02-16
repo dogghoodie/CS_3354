1b177d6df010837f6bf25ee8d3db0a2db7835de6
dogghoodie <dogghoodie@gmail.com>
2024-02-16 01:10:19 -0600

    Enabled Maven

---
+++ 1/src/main/java/JavaJSON.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONPaser;
+
+class JavaJSON {
+	public static void main(String[] args) {
+		
+	}
+}
