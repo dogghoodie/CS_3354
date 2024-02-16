35e4342a0b82a035dd363c9f9a13d2c9f3ddf039
dogghoodie <dogghoodie@gmail.com>
2024-02-16 05:05:42 -0600

    cleanup

--- 1/src/main/java/JavaJSON.java
+++ 1/src/main/java/JavaJSON.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 class JavaJSON {
 	public static void main(String[] args) {
 		// Welcome text / prompt	
-
 		System.out.println("\n - Walker Knapp - \n");
 		System.out.println("\nWelcome to JavaJSON\n");
 		System.out.println("Enter the file path of the");
 		System.out.println("file you would like to edit.\n");
 		System.out.print("Enter: ");
-
 		// Instantiate InputHandler to process user input
 		InputHandler handle = new InputHandler();
 		// Set input string to the input processed by handler
 		String input = handle.scan();
 		// Test handler - 
 		System.out.println(input);
-
 	}
 }
